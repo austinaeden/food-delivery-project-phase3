acb2b8951af304f4683fa43a9ecdfe392e36d1d1
austinaeden <austin.mbogo@student.moringaschool.com>
2023-09-06 21:37:52 +0300

    migrations update

---
+++ lib/app.py
@@ -0,0 +1,9 @@
+# alembic upgrade head
+# pip install faker
+# alembic init migrations
+# cd lib
+# alembic revision -m "empty init"
+# alembic upgrade head
+# alembic revision --autogenerate -m "created tables"
+# alembic upgrade head
+
